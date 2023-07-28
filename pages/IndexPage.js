19270ad411d32b5e1085c5139c93ccd20e4d1b8d
mwihoti <danielmwihoti@gmail.com>
2023-07-28 19:56:54 +0300

    blog-app

--- pages/IndexPage.js
+++ pages/IndexPage.js
@@ -53,47 +53,17 @@ const IndexPage = ({navigation}) => {
     </View>
   );
 }
- 
 const styles = StyleSheet.create({
-    
-  images: {
-    width: '100%',
-    height: 200,
-    resizeMode: 'cover',
-  },
-  headerTitle: {
-    fontSize: 40,
-    fontWeight: 'bold',
-    textAlign: 'left',
-    marginTop: 10,
-    color: 'white',
-    textShadowColor: 'rgba(0, 0, 0, 0.9)',
-    textShadowOffset: { width: -1, height: 1 },
-    textShadowRadius: 10,
-    position: 'absolute',
-    top: 140,
-    left: 20,
-  },
-  button: {
-    backgroundColor: '#1E90FF',
-    padding: 10,
-    borderRadius: 10,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginLeft: 10,
-  },
   container: {
     height: '100%',
-    padding: 20,
     backgroundColor: '#031031',
+    padding: 10,
   },
- 
+  input: {
+    color: '#fff',
+    fontSize: 20,
+  },
 });
+
  
 export default IndexPage;
